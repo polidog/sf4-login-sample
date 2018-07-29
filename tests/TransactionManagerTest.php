8d7f00276d3fa37a26bc7476d3f3d3ef08600151
polidog <polidogs@gmail.com>
2018-07-29 18:42:18 +0900

    Add TransactionManager test

---
+++ tests/TransactionManagerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Tests;
+
+
+use App\TransactionManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+class TransactionManagerTest extends TestCase
+{
+    private $entityManager;
+
+    protected function setUp()
+    {
+        $this->entityManager = $this->prophesize(EntityManagerInterface::class);
+    }
+
+
+    public function testBegin()
+    {
+        $tm = $this->getTransactionManager();
+        $tm->begin();
+
+        $this->entityManager->beginTransaction()
+            ->shouldHaveBeenCalled();
+    }
+
+    public function testCommit()
+    {
+        $tm = $this->getTransactionManager();
+        $tm->commit();
+
+        $this->entityManager->commit()
+            ->shouldHaveBeenCalled();
+    }
+
+    public function testRollback()
+    {
+        $tm = $this->getTransactionManager();
+        $tm->rollback();
+
+        $this->entityManager->rollback()
+            ->shouldHaveBeenCalled();
+    }
+
+
+    private function getTransactionManager()
+    {
+        return new TransactionManager($this->entityManager->reveal());
+    }
+}
