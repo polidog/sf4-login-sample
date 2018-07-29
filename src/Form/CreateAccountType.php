5caaa976a56f7d85dd9f4d9bf3b7f7dfbe56a624
polidog <polidogs@gmail.com>
2018-07-29 14:10:20 +0900

    change scrutinizer

--- src/Form/CreateAccountType.php
+++ src/Form/CreateAccountType.php
@@ -14,6 +14,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
+
 class CreateAccountType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -24,6 +27,7 @@ class CreateAccountType extends AbstractType
             ])
             ->add('email', EmailType::class, [
                 'label' => 'メールアドレス',
+                'constraints' => [new Assert\Email()]
             ])
             ->add('rawPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
